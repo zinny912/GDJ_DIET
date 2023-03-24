fd99243c8e96d5686d5922286bd2c3e9d0aa42dd
gmdiaal <gmdiaal@gmail.com>
2023-03-24 10:09:01 +0900

    123

--- src/main/java/com/goody/diet/order/OrderController.java
+++ src/main/java/com/goody/diet/order/OrderController.java
@@ -1,19 +1,48 @@
 package com.goody.diet.order;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.goody.diet.healthMachine.HealthMachineDTO;
 import com.goody.diet.member.MemberDTO;
+import com.goody.diet.study.StudyDTO;
 
 @Controller
 @RequestMapping("/order/*")
 public class OrderController {
 
+	@Autowired
+	OrderService orderService;
+	
 	@GetMapping("list")
-	public ModelAndView getOrderList(ModelAndView mv, MemberDTO memberDTO) {
+	public ModelAndView getOrderList(ModelAndView mv, MemberDTO memberDTO) throws Exception {
+//		MemberDTO memberDTO=(MemberDTO)session.getAttribute("sessionMember");
 		
 		return mv;
 	}
+	
+	@GetMapping("setOrderList")
+	public ModelAndView setOrderList(ModelAndView mv, HttpSession session) throws Exception {
+		//장바구니->주문
+		//상품->주문
+		OrderDTO orderDTO = new OrderDTO();
+		MemberDTO memberDTO=(MemberDTO)session.getAttribute("sessionMember");
+		orderDTO.setId(memberDTO.getId());
+		orderService.setOrderList(orderDTO);
+		return mv;
+	}
+	
+	@GetMapping("orderDetail")
+	public ModelAndView setOrderDetail(ModelAndView mv, OrderDetailDTO orderDetailDTO) throws Exception {
+		orderService.setOrderDetail(orderDetailDTO);
+	
+		return mv;
+	}
+
+	
 }
--- src/main/java/com/goody/diet/order/OrderDAO.java
+++ src/main/java/com/goody/diet/order/OrderDAO.java
@@ -2,21 +2,34 @@ package com.goody.diet.order;
 
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.goody.diet.healthMachine.HealthMachineDTO;
 import com.goody.diet.member.MemberDTO;
+import com.goody.diet.study.StudyDTO;
 
 @Repository
 public class OrderDAO {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	private final String NAMESPACE="com.goody.diet.order.OrderDAO.";
 
 	public OrderDTO getOrderList(MemberDTO memberDTO) {
 		
 		return null;
 	}
 	
-	public List<OrderDetailDTO> getOrderDetail(OrderDTO orderDTO) {
+	public int setOrderList(OrderDTO orderDTO) throws Exception {
+		return 0;
+	}
+	
+	public int setOrderDetail(OrderDetailDTO orderDetailDTO) throws Exception {
 		
-		return null;
+		return sqlSession.insert(NAMESPACE+"setOrderDetail", orderDetailDTO);
 	}
 	
 }
