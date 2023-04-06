53bc435fb9d7287a78f3eec7a6d6736788e2cda5
gmdiaal <gmdiaal@gmail.com>
2023-04-06 10:30:30 +0900

    123

--- src/main/java/com/goody/diet/order/OrderController.java
+++ src/main/java/com/goody/diet/order/OrderController.java
@@ -30,63 +30,91 @@ public class OrderController {
 	@GetMapping("list")
 	public ModelAndView getOrderList(ModelAndView mv, HttpSession session) throws Exception {
 		System.out.println("------------------오더리스트-----------------");
+		//ajax나, sql문 아니면 order랑 cart랑 연결이 안댐..
 		MemberDTO memberDTO=(MemberDTO)session.getAttribute("sessionMember");
-		List<OrderDTO> orderDTOs=orderService.getOrderList(memberDTO);	//여기 다 resultmap으로 detail까지 다 담김.
-		mv.addObject("orderList", orderDTOs);
+		List<OrderDTO> orderDTOs=orderService.getOrderList(memberDTO);	
+
+		
+		mv.addObject("orderDTOs", orderDTOs);
 		mv.setViewName("/order/orderListPage");
 		return mv;
 	}
-	
-	
-	@GetMapping("listDetail") 
-	public ModelAndView getOrderDetailList(ModelAndView mv, OrderDetailDTO orderDetailDTO) throws Exception {
-		System.out.println("------------------오더디테일-----------------");	
-		System.out.println(orderDetailDTO.getOrderDetailNum());
-		orderDetailDTO=orderService.getOrderDetail(orderDetailDTO);
-		
-		if(orderDetailDTO.getMachineNum()!=null) {			
-			HealthMachineDTO healthMachineDTO = orderService.getHealthMachineForCartAndOrder(orderDetailDTO);
-			String healthMachine=healthMachineDTO.getMachineName();
-			//getRealHealthMachineForCartAndOrder	//상품 색상,무게 등 불러옴
-			RealHealthMachineDTO realHealthMachineDTO = orderService.getRealHealthMachineForCartAndOrder(orderDetailDTO);
-			
-			//링크용
-			mv.addObject("healthMachineDTO", healthMachineDTO);
-			//상품 요약만 string으로
-			if(realHealthMachineDTO!=null) {			
-				if(healthMachineDTO.getOption1()!=null) {
-					healthMachine=healthMachine+", "+realHealthMachineDTO.getOptName1();
-				}
-				if(healthMachineDTO.getOption2()!=null) {
-					healthMachine=healthMachine+", "+realHealthMachineDTO.getOptName2();
-				}
-				if(healthMachineDTO.getOption3()!=null) {
-					healthMachine=healthMachine+", "+realHealthMachineDTO.getOptName3();
-				}
-				if(healthMachineDTO.getOption4()!=null) {
-					healthMachine=healthMachine+", "+realHealthMachineDTO.getOptName4();
-				}
-			}
-			mv.addObject("healthMachine", healthMachine);
-			
-			//이미지만
-//			if(healthMachineDTO.getHealthMachineImgDTOs().get(0)!=null) {//만약 이미지 없음				
-//				HealthMachineImgDTO healthMachineImgDTO=healthMachineDTO.getHealthMachineImgDTOs().get(0);
-//				mv.addObject("HealthMachineImgDTO", healthMachineImgDTO);
-//			}
+	@GetMapping("cartList")
+	public ModelAndView getCartList(ModelAndView mv, OrderDTO orderDTO) throws Exception {
+		List<CartDTO> cartDTOs = orderService.getCartList(orderDTO);
+		List<StudyDTO> studyDTOs = new ArrayList<StudyDTO>(); 
+		List<HealthMachineDTO> HealthMachineDTOs = new ArrayList<HealthMachineDTO>();
+		for(CartDTO cartDTO:cartDTOs) {
+			studyDTOs.add(orderService.getStudy(cartDTO));
+			HealthMachineDTOs.add(orderService.getHealthMachine(cartDTO));
+			//밖에서 cartDTO랑 어떻게 매치시킴? if문으로 num이랑 하자..
 		}
-		//getStudyForCartAndOrder
-		if(orderDetailDTO.getStudyNum()!=null) {	
-			StudyDTO studyDTO = orderService.getStudyForCartAndOrder(orderDetailDTO);
-			mv.addObject("studyDTO", studyDTO);
-		}
-		
-		  
-		mv.setViewName("/order/detailAjax");
-		return mv; 
+		mv.setViewName("../common/ajaxResult");
+		mv.addObject("cartDTOs", cartDTOs);
+		mv.addObject("studyDTOs", studyDTOs);
+		mv.addObject("HealthMachineDTOs", HealthMachineDTOs);
+		return mv;
 	}
+//	public ModelAndView getStudy(ModelAndView mv, CartDTO cartDTO) throws Exception {
+//		orderService.getStudy(cartDTO);
+//		return 
+//	}	
+//	public ModelAndView getRealHealthMachine(ModelAndView mv, CartDTO cartDTO) throws Exception {
+//		orderService.getRealHealthMachine(cartDTO);
+//		return
+//	}		
+
+	
+////구////구////구////구////구////구////구////구////구////구////구////구////구////구////구
+//	@GetMapping("listDetail") 
+//	public ModelAndView getOrderDetailList(ModelAndView mv, OrderDetailDTO orderDetailDTO) throws Exception {
+//		System.out.println("------------------오더디테일-----------------");	
+//		System.out.println(orderDetailDTO.getOrderDetailNum());
+//		orderDetailDTO=orderService.getOrderDetail(orderDetailDTO);
+//		
+//		if(orderDetailDTO.getMachineNum()!=null) {			
+//			HealthMachineDTO healthMachineDTO = orderService.getHealthMachineForCartAndOrder(orderDetailDTO);
+//			String healthMachine=healthMachineDTO.getMachineName();
+//			//getRealHealthMachineForCartAndOrder	//상품 색상,무게 등 불러옴
+//			RealHealthMachineDTO realHealthMachineDTO = orderService.getRealHealthMachineForCartAndOrder(orderDetailDTO);
+//			
+//			//링크용
+//			mv.addObject("healthMachineDTO", healthMachineDTO);
+//			//상품 요약만 string으로
+//			if(realHealthMachineDTO!=null) {			
+//				if(healthMachineDTO.getOption1()!=null) {
+//					healthMachine=healthMachine+", "+realHealthMachineDTO.getOptName1();
+//				}
+//				if(healthMachineDTO.getOption2()!=null) {
+//					healthMachine=healthMachine+", "+realHealthMachineDTO.getOptName2();
+//				}
+//				if(healthMachineDTO.getOption3()!=null) {
+//					healthMachine=healthMachine+", "+realHealthMachineDTO.getOptName3();
+//				}
+//				if(healthMachineDTO.getOption4()!=null) {
+//					healthMachine=healthMachine+", "+realHealthMachineDTO.getOptName4();
+//				}
+//			}
+//			mv.addObject("healthMachine", healthMachine);
+//			
+//			//이미지만
+////			if(healthMachineDTO.getHealthMachineImgDTOs().get(0)!=null) {//만약 이미지 없음				
+////				HealthMachineImgDTO healthMachineImgDTO=healthMachineDTO.getHealthMachineImgDTOs().get(0);
+////				mv.addObject("HealthMachineImgDTO", healthMachineImgDTO);
+////			}
+//		}
+//		//getStudyForCartAndOrder
+//		if(orderDetailDTO.getStudyNum()!=null) {	
+//			StudyDTO studyDTO = orderService.getStudyForCartAndOrder(orderDetailDTO);
+//			mv.addObject("studyDTO", studyDTO);
+//		}
+//		
+//		  
+//		mv.setViewName("/order/detailAjax");
+//		return mv; 
+//	}
 	  
-	  
+	///////////////////////배송지///////////////////////
 	@GetMapping("newPaymentOrder")
 	public ModelAndView getPaymentOrder(HttpSession session, DeliveryDTO deliveryDTO, ModelAndView mv) throws Exception {
 		System.out.println("-------------------getPaymentOrder-------------------");
@@ -102,25 +130,13 @@ public class OrderController {
 		mv.setViewName("/order/paymentPageAddressAjax");
 		return mv;
 	}
+	
+	///////////////////////test 주문페이지/////////////////////
 	@GetMapping("checkout")
 	public ModelAndView setOrder(ModelAndView mv) throws Exception {
-
 		mv.setViewName("/order/paymentPage");
 		return mv;
 	}	
-//	@GetMapping("setOrder")
-//	public ModelAndView setOrder(ModelAndView mv, HttpSession session, List<CartDTO> cartDTOs) throws Exception {
-//		//여기서 다처리하자..
-//		//장바구니->주문 //checked만 주문하려면 List로 받아야 함. (x id->cart->product 검색 안대..)
-//		//1. 상품->주문(장바구니)->주문? 주문도중 취소하면 장바구니에 남아야하는데 바로주문도 담기네...? //쿠팡에선 그냥 장바구니로 들어가네..
-//		//2. 상품->주문
-//
-//		//cartDTO는 num만 보내주면 대..
-//		orderService.setOrder((MemberDTO)session.getAttribute("sessionMember"), cartDTOs);
-//		//결제페이지로..가야댐.. 그냥 주문된걸로 칠께..
-//		mv.setViewName("./paymentPage");
-//		return mv;
-//	}
 
 
 	
