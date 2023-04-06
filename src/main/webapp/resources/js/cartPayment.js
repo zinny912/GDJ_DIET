d3f7f9718d20d9770e830642066ffb912e0cdf97
kimtaehyeun <rlaxogus2799@naver.com>
2023-04-06 09:24:01 +0900

    1

--- src/main/webapp/resources/js/cartPayment.js
+++ src/main/webapp/resources/js/cartPayment.js
@@ -1,5 +1,7 @@
 const checkAll = document.getElementById("checkAll")
 const checks = document.getElementsByClassName("checks")
+let deliveryPrice=$("#deliveryPrice").attr("data-deliveryPrice")*1;
+
 
 checkAll.addEventListener("click",function(){
     for(let i=0;i<checks.length;i++){
@@ -112,3 +114,67 @@ function checkDelete(){
 	    });
 	  }
 	}
+
+// 태현-------------------------------
+let TotalPrice=0;
+let Checked =true;
+$("#totalCost").html(TotalPrice+deliveryPrice);
+
+$("#cartMachineList").on("click",".checks",function(){
+	let cartPrice = $(this).parent().parent().find("#cartPrice").html()*1;
+	Checked = $(this).prop("checked");
+	if(Checked==true){//누른 후 체크라면(원래는 노체크)
+		TotalPrice=TotalPrice+cartPrice;
+	}
+	else{
+		TotalPrice=TotalPrice-cartPrice;
+	}
+	$("#totalPrice").html(TotalPrice);
+	$("#totalCost").html(TotalPrice+deliveryPrice);
+})
+$(".countBtn").on("click",".plusbtn",function(){
+	let body =$(this).parent().parent().parent()
+
+	//1. count구하기
+	let count=body.find("#count").html()*1;
+	count=count+1
+	body.find("#count").html(count)
+
+	//2. price올리기
+	let cartPrice=body.find("#cartPrice").html()*1;
+	cartPrice=cartPrice/(count-1)*count;
+	body.find("#cartPrice").html(cartPrice);
+	//3. 총합계 update
+	Checked = $(this).parent().parent().parent().find(".checks").prop("checked");
+	if(Checked==true){
+		TotalPrice=TotalPrice+ cartPrice/count;
+		$("#totalPrice").html(TotalPrice);
+		$("#totalCost").html(TotalPrice+deliveryPrice);
+	}
+	
+})
+$(".countBtn").on("click", ".minusbtn", function () {
+	let body = $(this).parent().parent().parent()
+	//1. count구하기
+	let count = body.find("#count").html() * 1;
+	if (count * 1 <= 1) {
+		alert("상품은 최소 1개 이상 있어야 합니다.")
+		return;
+	}
+	
+		count=count-1
+		body.find("#count").html(count)
+		//2. price내리기
+		let cartPrice=body.find("#cartPrice").html()*1;
+		cartPrice=cartPrice/(count+1)*count
+		body.find("#cartPrice").html(cartPrice);
+		
+		if(Checked==true){
+			TotalPrice=TotalPrice- cartPrice/count;
+			$("#totalPrice").html(TotalPrice);
+			$("#totalCost").html(TotalPrice+deliveryPrice);
+		}
+	
+})
+
+// -------------------------------
