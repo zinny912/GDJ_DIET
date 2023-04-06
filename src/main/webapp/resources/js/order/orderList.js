43e4bb61d45d79e08f6cf369794fe91e3fb3b1f8
gmdiaal <gmdiaal@gmail.com>
2023-04-06 17:38:19 +0900

    123

--- src/main/webapp/resources/js/order/orderList.js
+++ src/main/webapp/resources/js/order/orderList.js
@@ -1,35 +1,24 @@
 
 //orderNum으로 cartList불러오기
 let orderLeng = document.getElementsByClassName("order_number")
-{
-    for(let i=0; i<orderLeng.length;i++){
-        $.ajax({
-            type:"GET",
-            url:"/order/cartList",
-            data:{
-                orderNum:orderLeng[i].getAttribute("data-orderNum")
-            },
-            success: (res)=>{
 
-                $('.cartListIn_'+orderLeng[i].getAttribute("data-orderNum")).html(res.trim())
-            }
-        })
+// //장바구니에 추가하기
+// {
+//     for(let i=0; i<orderLeng.length;i++){
+//         $.ajax({
+//             type:"GET",
+//             url:"/order/cartList",
+//             data:{
+//                 orderNum:orderLeng[i].getAttribute("data-orderNum")
+//             },
+//             success: (res)=>{
 
-        // if(i==orderLeng.length-1){
-        //     console.log("도착")
+//                 $('.cartListIn_'+orderLeng[i].getAttribute("data-orderNum")).html(res.trim())
+//             }
+//         })
 
-        // let popoverTriggerList = document.getElementsByClassName('to-cart-popper')
-        // let popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl, { container: 'body',
-        // title: "<a href='/cart/cartList' type='button'>버튼</a>", content:'<a href="" class="btn btn-danger">장바구니로</a>', html: true}))
-      
-
-        //     // popoverTriggerList = document.getElementsByClassName('to-cart-popper')
-        //     // popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl, { container: 'body',
-        //     // title: "<a href='/cart/cartList' type='button'>버튼</a>", content:'  <a href="" class="btn btn-danger">장바구니로</a>', html: true}))
-            
-        // }
-    }
-}
+//     }
+// }
 
 $(document).ajaxComplete(function(){
         let popoverTriggerList = document.getElementsByClassName('to-cart-popper')
